ddb5eb887163f3ca26f23187017e66cc26349863
SeattleDantz <tricia@curo-software.com>
2021-04-24 22:09:30 -0700

    Added Github repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        {" "}
+        This project is coded by Tricia Brigham and is{" "}
+        <a
+          href="https://github.com/SeattleDantz/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-source on Github
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
