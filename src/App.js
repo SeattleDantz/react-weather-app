bcafcc73cc3ad0b31b7ea9d072e38f1c5163f92f
SeattleDantz <tricia@curo-software.com>
2021-04-30 20:43:28 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        {" "}
-        This project is coded by Tricia Brigham and is{" "}
-        <a
-          href="https://github.com/SeattleDantz/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-source on Github
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          {" "}
+          <a
+            href="https://github.com/SeattleDantz/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open source
+          </a>{" "}
+          project by Tricia Brigham.
+        </footer>
+      </div>
     </div>
   );
 }
