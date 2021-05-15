e3bbbb15f26176918d1766a434b7456d8b382892
SeattleDantz <tricia@curo-software.com>
2021-05-14 22:58:14 -0700

    Start styling

--- src/Weather.js
+++ src/Weather.js
@@ -4,38 +4,45 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <form>
+      <form className="enter-city">
         <div className="row">
           <div className="col-9">
             <input
               type="search"
               placeholder="Enter a city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
             <input
               type="submit"
               placeholder="submit"
-              className="btn btn-primary"
+              className="btn btn-primary w-100"
             />
           </div>
         </div>
       </form>
 
-      <h3>Woodinville</h3>
+      <h1>Woodinville</h1>
       <ul>
         <li>Friday 21:00</li>
         <li>Clear</li>
       </ul>
 
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            alt="Clear"
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-          />
-          57 ℉
+          <div className="clearfix">
+            <img
+              alt="Clear"
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">57</span>{" "}
+              <span className="units">℉</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
