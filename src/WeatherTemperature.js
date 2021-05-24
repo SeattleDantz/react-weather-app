fa678e74ff044e1f48418c390583e15f2ae2275a
SeattleDantz <tricia@curo-software.com>
2021-05-23 18:20:20 -0700

    Removed ability to convert from F to C

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,35 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("farenheit");
-  function convertToCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-  function convertToFarenheit(event) {
-    event.preventDefault();
-    setUnit("farenheit");
-  }
-  if (unit === "farenheit") {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.farenheit)}</span>{" "}
-        <span className="units">℉</span>
-      </div>
-    );
-  } else {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">
-          {Math.round((5 / 9) * (Math.round(props.farenheit) - 32))}
-        </span>{" "}
-        <span className="units">
-          <a href="/" onClick={convertToFarenheit}>
-            ℉{" "}
-          </a>
-          | ℃
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.farenheit)}</span>{" "}
+      <span className="units">℉</span>
+    </div>
+  );
 }
